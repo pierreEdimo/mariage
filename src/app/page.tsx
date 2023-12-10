c5eaf101c867c61b6f6afc97a12c07fb8271cc29
pierre.nkoe <pierre.nkoe@vema-eg.de>
2023-12-10 19:24:15 +0100

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,98 @@
 import Image from 'next/image'
+import {AppBar} from "@/app/shared/appbar";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <main className="relative">
+            <AppBar/>
+            <section className="full-height w-full bg-gray-500">
+                <div>
+                    SAVE THE DATE
+                </div>
+            </section>
+            <section className="slanded-section negative-margin">
+                <div className="content-height m-auto w-2/3 flex items-center  skw">
+                    <div className="w-1/2 text-black">
+                        <p className="text-2xl font-bold">He asker her & she said yes!</p>
+                        <br/>
+                        <p>Phasellus a sapien tincidunt nunc dapibus mollis. Etiam ultricies bibendum sodales. Nulla
+                            libero tortor, auctor at elit ut, pharetra placerat libero. Aenean gravida in libero sed
+                            tincidunt. Nulla non mattis nisl, in imperdiet justo. Proin metus mauris, gravida vel ante
+                            et, ornare laoreet libero sed ut quam vitae. Aenean aliquam augue turpis, in tempor elit
+                            condimentum sit amet. Aenean aliquam augue turpis, in tempor elit condimentum sit amet.
+                            Phasellus a sapien tincidunt nunc dapibus mollis. Etiam ultricies bibendum sodales.</p>
+                    </div>
+                    <div className=" w-1/3 image-height negative-margin bg-red-500">
+                        <p>test</p>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-gray-500 w-full negative-margin">
+                <div className="m-auto w-2/3 py-24 standard-height">
+                    <div className="h-full w-full flex items-center justify-center flex-col gap-20">
+                        <h1 className="text-4xl text-white font-bold">We're getting married</h1>
+                        <div className="custom-container">
+                            <div className="inside-container">
+                                <div className="container-content flex">
+                                    <div
+                                        className="flex-1 flex items-center flex-col justify-center bg-transparent text-white">
+                                        <p className="font-bold text-8xl">Save</p>
+                                        <p className="text-xl">THE</p>
+                                        <p className="font-bold text-8xl">Date</p>
+                                    </div>
+                                    <div
+                                        className="flex-1 transparent-color flex items-center flex-col justify-center top-border gap-12 text-white">
+                                        <div className="flex flex-col gap-2 p-2">
+                                            <p className="text-center"><span
+                                                className="font-bold text-3xl">Franklin</span> & <span
+                                                className="font-bold text-3xl">Vanessa</span></p>
+                                            <p className="text-center">Request the honor of your presence on their
+                                                wedding
+                                                day</p>
+                                        </div>
+                                        <div className="text-center">
+                                            <p className="font-bold text-2xl">September 22, 2022</p>
+                                            <p>at 03:00 pm</p>
+                                        </div>
+                                        <div className="text-center">
+                                            <p>At Birchwood Church</p>
+                                            <p>4181 Birchwood Ave Seal Beach, CA</p>
+                                            <p>Reception to follow</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-blue-400 py-24 w-full standard-height relative">
+                <div className="w-2/3 m-auto">
+                    <div className="w-full flex ">
+                        <div className="flex-1 flex items-center text-white border-r-2 justify-center flex-col">
+                            <p className="text-4xl font-bold">a</p>
+                            <p className="text-3xl font-bold">Ceremony</p>
+                            <p>03:00 PM - 04:00 PM</p>
+                        </div>
+                        <div className="flex-1 flex items-center text-white justify-center flex-col">
+                            <p className="text-4xl font-bold" >h</p>
+                            <p className="text-3xl font-bold">Photoshoot</p>
+                            <p>05:00 PM - 06:30 PM</p>
+                        </div>
+                        <div className="flex-1 flex items-center text-white justify-center flex-col border-l-2">
+                            <p className="text-4xl font-bold">b</p>
+                            <p className="text-3xl font-bold">Reception</p>
+                            <p>07:00 PM - 01:00 PM</p>
+                        </div>
+                    </div>
+                </div>
+                <div className=" m-auto shadow-2xl p-2 bg-white card z-10 absolute transform -translate-x-1/2  h-96 left-1/2">
+                    <p>test</p>
+                </div>
+            </section>
+            <section className="slanded-section negative-margin bg-white standard-height">
+                <p>test</p>
+            </section>
+        </main>
+    )
 }
